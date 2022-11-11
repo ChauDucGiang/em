880495336a53247379f098a51ac5cb334cef30ae
chauhoang <chauhv@feelsyncsystem.vn>
2022-11-11 14:16:30 +0700

    em

---
+++ love.js
@@ -0,0 +1,7 @@
+var love = document.getElementById("love");
+var no_love =document.getElementById("no-love");
+
+love.addEventListener("click", myFunction);
+function myFunction(){
+ alert('Anh Biết Mà ahihi')
+}
